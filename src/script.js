a79cbf9900db12bb8fa60e68a33e7d449a29d430
jeanrabello <jean.rabello.prog@gmail.com>
2021-12-14 15:17:32 -0300

    Criação da função ChronPause

--- src/script.js
+++ src/script.js
@@ -1,14 +1,15 @@
-function CronStart() {
-    console.log("Iniciando cronômetro");
-    
-    var hours = document.getElementById("cron-hours");
-    var minutes = document.getElementById("cron-minutes");
-    var seconds = document.getElementById("cron-seconds");
+var chronometer;
+var h = 0, m = 0, s = 0;
 
-    var h = 0, m = 0, s = 0;
+function ChronStart() {
+    console.log("Iniciando cronômetro");
+
+    var hours = document.getElementById("chron-hours");
+    var minutes = document.getElementById("chron-minutes");
+    var seconds = document.getElementById("chron-seconds");
 
     console.log("Iniciando Intervalo");
-    setInterval(function () {
+    chronometer = setInterval(function () {
 
         hours.innerHTML = h < 10 ? "0" + h : h;
         minutes.innerHTML = m < 10 ? "0" + m : m;
@@ -33,6 +34,7 @@ function CronStart() {
     }, 1000);
 }
 
-function CronPause() {
-    
+function ChronPause() {
+    console.log("Pausando cronômetro");
+    clearInterval(chronometer);
 }
